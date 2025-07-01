045e68e745527f845f39a750f14fbfbacddcc538
bryanbernardo22 <bryan.bernardo.santos@escola.pr.gov.br>
2025-07-01 17:12:17 -0300

    bryan

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(categoria, pergunta, resposta){
+    'CÊNCIAS'
+    'Como é feito a fotossintese?'
+    'Pelo Sol'
+}
+
+criaCartao(
+    'PROGRAMAÇÃO'
+    'O que é JavaScript?'
+    'E uma linguagem de programação'
+)
+
+ciaCartao(
+    'GEOGRAFIA'
+    'Qual a Capita'
+)
